6b83e48d0d0d871329c771d02f870dc8ec135596
devatha k <k.n.devs@gmail.com>
2016-01-03 14:49:50 -0500

    rendr json

--- routes/items.js
+++ routes/items.js
@@ -5,7 +5,10 @@ var Item = require('../models/items');
 router.get('/', function(req, res) {
 	console.log("got a GET")
 Item.find({}).exec(function(err, items) {
-    res.render('items/index', {items: items});
+    // res.render('items/index', {items: items});
+
+    // checking if this works below and it does.
+    res.json({items: items});
 });
 });
 
