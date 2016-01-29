44f327b4808bf17d6328185510addee10e256271
devatha k <k.n.devs@gmail.com>
2016-01-29 12:34:18 -0500

    friendly

--- routes/items.js
+++ routes/items.js
@@ -10,8 +10,6 @@ router.get('/', function(req, res) {
 // for getting data
 router.get('/blooms', function(req, res) {
   Item.find({}).exec(function(err, items) {
-    // console.log("data from the db")
-    // console.log(items)
     res.json(items);
   });
 });
@@ -19,10 +17,7 @@ router.get('/blooms', function(req, res) {
 
 // for posting data
 router.post('/blooms', function(req, res) {
-  console.log(req.body);
-	Item.create(req.body, function(err, item) {
-      // console.log("from post the data")
-	    // console.log(item);   
+	Item.create(req.body, function(err, item) {  
 	    res.json(item)
   });
 });
@@ -30,11 +25,7 @@ router.post('/blooms', function(req, res) {
 
 // for deleting data
 router.delete('/blooms/:id', function(req, res) {
-	// var id = req.params.id
- //  console.log(id);
-   // console.log(req.params.id);
   Item.findByIdAndRemove(req.params.id, function(err, doc){
-  	// console.log(doc);
   	res.json(doc)
   })
 });
@@ -43,18 +34,14 @@ router.delete('/blooms/:id', function(req, res) {
 
 // for editing and updating data
 router.get('/blooms/:id', function(req, res) {
-  // console.log(req.params.id);
   Item.findById(req.params.id, function(err, item){
-  // console.log(item)
   res.json(item)
   })
 });
 
 router.put('/blooms/:id', function(req, res) {
   console.log("update please")
-  // console.log(req.params.id);
-  // console.log(req.body.body);
-  // console.log(req.body.tagline);
+
   Item.findById(req.params.id, function(err, item) {
     item.body = req.body.body;
     item.tagline = req.body.tagline;
@@ -74,10 +61,7 @@ router.put('/blooms/:id', function(req, res) {
 // ******** search functionality for the searchbox
 
 router.get('/search?/', function(req, res) {
-  console.log("??")
-  console.log(req.query.genres)
   Item.find({genres: new RegExp(req.query.genres, "i")},function(err, items) {
-    console.log(items)
       res.json(items) 
   });
 });
--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,6 @@ var Post = require('../models/posts');
 router.get('/', function(req, res) {
   Post.find({}).exec(function(err, posts) {
       res.render('posts/index', {posts: posts});
-      //below code works if you are sending through sendfile
-       // res.sendFile('/views/posts/index.html', {posts: posts}); 
   });
 });
 
@@ -17,9 +15,7 @@ router.get('/', function(req, res) {
 
 // for posting data
 router.post('/jupiter', function(req, res) {
- // console.log(req.body);
  Post.create(req.body, function(err, post) {
- // console.log(post)
 res.json(post)
 });
 })
@@ -35,8 +31,6 @@ router.delete('/:id', function(req, res) {
 router.get('/doublejax', function(req, res) {
   Post.find({}).exec(function(err, posts) {
       res.json(posts)
-      //below code works if you are sending through sendfile
-       // res.sendFile('/views/posts/index.html', {posts: posts}); 
   });
 });
 
@@ -44,8 +38,6 @@ router.get('/doublejax', function(req, res) {
 // forsearch  ***************************
 
 router.get('/search?/', function(req, res) {
-  console.log("??")
-  console.log(req.query.postname)
   Post.find({postname: new RegExp(req.query.postname, "i")},function(err, posts) {
       res.json(posts) 
   });
